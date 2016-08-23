9a661c359981cf3aadea1054fd67bcd4e59e8e30
snehakanekar <kanekarsneha2403@gmail.com>
2016-08-23 12:47:05 +0530

    update sql file

--- Config/createdbtables.sql
+++ Config/createdbtables.sql
@@ -5,3 +5,5 @@ create database onlinemoviebookingsystem;
 use onlinemoviebookingsystem;
 
 create table movie(movieCode varchar(255),movieName varchar(255),showDate varchar(255), lang varchar(255));
+
+GRANT ALL PRIVILEGES ON onlinemoviebookingsystem.* TO 'root'@'%' IDENTIFIED BY 'root';
